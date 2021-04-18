75da971a8f14f7088975789478ea2f188640eb63
Brandon Kennedy <brandk94@uw.edu>
2021-04-18 16:08:32 -0700

    Test Commit to Brandon

--- app/src/main/java/edu/uw/tcss450/team8tcss450/MasterTestClass.java
+++ app/src/main/java/edu/uw/tcss450/team8tcss450/MasterTestClass.java
@@ -1,5 +1,5 @@
 package edu.uw.tcss450.team8tcss450;
 
-public class TestClass2 {
+public class MasterTestClass {
     // This is for Brandon Branch.
 }
--- app/src/main/java/edu/uw/tcss450/team8tcss450/testClass.java
+++ app/src/main/java/edu/uw/tcss450/team8tcss450/testClass.java
@@ -1,5 +1,5 @@
 package edu.uw.tcss450.team8tcss450;
 
 public class testClass {
-
+    // This is for Brandon branch.
 }

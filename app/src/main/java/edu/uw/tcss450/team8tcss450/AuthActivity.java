13daa846b7a084fc3ebd2f869a77cc5f1cd34b7a
Marc Perez <marc.perez.dev@gmail.com>
2021-04-22 14:54:53 -0700

    Starter fragments in assigned packages created.

---
+++ app/src/main/java/edu/uw/tcss450/team8tcss450/AuthActivity.java
@@ -0,0 +1,15 @@
+package edu.uw.tcss450.team8tcss450;
+
+import android.os.Bundle;
+// TODO filler class, replace as needed
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/edu/uw/tcss450/team8tcss450/ui/signin/SignInFragment.java
@@ -0,0 +1,29 @@
+package edu.uw.tcss450.team8tcss450.ui.signin;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.team8tcss450.R;
+
+/**
+ * TODO Filler Class, alter as needed.
+ * A simple {@link Fragment} subclass.
+ */
+public class SignInFragment extends Fragment {
+
+    public SignInFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+}

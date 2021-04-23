71c4f1161312bf68b5d293eb05f2e0a41c97f5ff
Tien Tang <tientang@uw.edu>
2021-04-22 20:53:05 -0700

    added a welcome home textview

--- app/src/main/java/edu/uw/tcss450/team8tcss450/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uw/tcss450/team8tcss450/ui/home/HomeFragment.java
@@ -2,6 +2,7 @@ package edu.uw.tcss450.team8tcss450.ui.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,12 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450.team8tcss450.R;
+import edu.uw.tcss450.team8tcss450.databinding.FragmentHomeBinding;
 
 /**
  * TODO Filler Class, alter as needed.
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,5 +29,14 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FragmentHomeBinding binding =FragmentHomeBinding.bind(getView());
+
+        binding.textView.setText("Welcome Home");
     }
 }

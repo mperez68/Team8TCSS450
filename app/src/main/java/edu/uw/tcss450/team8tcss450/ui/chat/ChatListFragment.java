2314b0d67d1982d2e5b5eef102613b10d2557fa7
Marc Perez <marc.perez.dev@gmail.com>
2021-04-29 02:42:23 -0700

    Had to revert, round 2 of adding conversation list.

---
+++ app/src/main/java/edu/uw/tcss450/team8tcss450/ui/chat/ChatListFragment.java
@@ -0,0 +1,39 @@
+package edu.uw.tcss450.team8tcss450.ui.chat;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.team8tcss450.R;
+import edu.uw.tcss450.team8tcss450.databinding.FragmentChatListBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatListFragment extends Fragment {
+
+    public FragmentChatListBinding binding;
+
+    public ChatListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+}

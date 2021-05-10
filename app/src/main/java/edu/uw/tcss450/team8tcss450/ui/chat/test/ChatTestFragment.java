e5b3776bf73fe990898bcbf5bfa543e5d3a33ca6
NicoRoberts <33466452+NicoRoberts@users.noreply.github.com>
2021-05-10 15:41:55 -0700

    intial chatroom

---
+++ app/src/main/java/edu/uw/tcss450/team8tcss450/ui/chat/test/ChatTestFragment.java
@@ -0,0 +1,103 @@
+package edu.uw.tcss450.team8tcss450.ui.chat.test;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.team8tcss450.R;
+import edu.uw.tcss450.team8tcss450.databinding.FragmentChatTestBinding;
+import edu.uw.tcss450.team8tcss450.model.UserInfoViewModel;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatTestFragment extends Fragment {
+
+
+
+    //The chat ID for "global" chat
+    private static final int HARD_CODED_CHAT_ID = 1;
+
+    private ChatTestViewModel mChatModel;
+    private UserInfoViewModel mUserModel;
+    private ChatTestSendViewModel mSendModel;
+
+    public ChatTestFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewModelProvider provider = new ViewModelProvider(getActivity());
+        mUserModel = provider.get(UserInfoViewModel.class);
+        mChatModel = provider.get(ChatTestViewModel.class);
+        mChatModel.getFirstMessages(HARD_CODED_CHAT_ID, mUserModel.getJWT().toString()); //edited here from mUserModel.getmJWT from Lab 5
+        mSendModel = provider.get(ChatTestSendViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FragmentChatTestBinding binding = FragmentChatTestBinding.bind(getView());
+
+        //SetRefreshing shows the internal Swiper view progress bar. Show this until messages load
+        binding.swipeContainer.setRefreshing(true);
+
+        final RecyclerView rv = binding.recyclerMessages;
+        //Set the Adapter to hold a reference to the list FOR THIS chat ID that the ViewModel
+        //holds.
+        rv.setAdapter(new ChatTestRecyclerViewAdapter(
+                mChatModel.getMessageListByChatId(HARD_CODED_CHAT_ID),
+                mUserModel.getEmail()));
+
+
+        //When the user scrolls to the top of the RV, the swiper list will "refresh"
+        //The user is out of messages, go out to the service and get more
+        binding.swipeContainer.setOnRefreshListener(() -> {
+            mChatModel.getNextMessages(HARD_CODED_CHAT_ID, mUserModel.getJWT().toString()); //edited here from mUserModel.getmJWT from Lab 5
+        });
+
+        mChatModel.addMessageObserver(HARD_CODED_CHAT_ID, getViewLifecycleOwner(),
+                list -> {
+                    /*
+                     * This solution needs work on the scroll position. As a group,
+                     * you will need to come up with some solution to manage the
+                     * recyclerview scroll position. You also should consider a
+                     * solution for when the keyboard is on the screen.
+                     */
+                    //inform the RV that the underlying list has (possibly) changed
+                    rv.getAdapter().notifyDataSetChanged();
+                    rv.scrollToPosition(rv.getAdapter().getItemCount() - 1);
+                    binding.swipeContainer.setRefreshing(false);
+                });
+
+
+        //Send button was clicked. Send the message via the SendViewModel
+        binding.buttonSend.setOnClickListener(button -> {
+            mSendModel.sendMessage(HARD_CODED_CHAT_ID,
+                    mUserModel.getJWT().toString(), //edited here from mUserModel.getmJWT from Lab 5,
+                    binding.editMessage.getText().toString());
+        });
+        //when we get the response back from the server, clear the edittext
+        mSendModel.addResponseObserver(getViewLifecycleOwner(), response ->
+                binding.editMessage.setText(""));
+
+    }
+}
---
+++ app/src/main/java/edu/uw/tcss450/team8tcss450/ui/chat/test/ChatTestMessage.java
@@ -0,0 +1,66 @@
+package edu.uw.tcss450.team8tcss450.ui.chat.test;
+
+import androidx.annotation.Nullable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ChatTestMessage {
+
+    private final int mMessageId;
+    private final String mMessage;
+    private final String mSender;
+    private final String mTimeStamp;
+
+    public ChatTestMessage(int messageId, String message, String sender, String timeStamp) {
+        mMessageId = messageId;
+        mMessage = message;
+        mSender = sender;
+        mTimeStamp = timeStamp;
+    }
+
+    /**
+     * Static factory method to turn a properly formatted JSON String into a
+     * ChatMessage object.
+     * @param cmAsJson the String to be parsed into a ChatMessage Object.
+     * @return a ChatMessage Object with the details contained in the JSON String.
+     * @throws JSONException when cmAsString cannot be parsed into a ChatMessage.
+     */
+    public static ChatTestMessage createFromJsonString(final String cmAsJson) throws JSONException {
+        final JSONObject msg = new JSONObject(cmAsJson);
+        return new ChatTestMessage(msg.getInt("messageid"),
+                msg.getString("message"),
+                msg.getString("email"),
+                msg.getString("timestamp"));
+    }
+
+    public String getMessage() {
+        return mMessage;
+    }
+
+    public String getSender() {
+        return mSender;
+    }
+
+    public String getTimeStamp() {
+        return mTimeStamp;
+    }
+
+    public int getMessageId() {
+        return mMessageId;
+    }
+
+    /**
+     * Provides equality solely based on MessageId.
+     * @param other the other object to check for equality
+     * @return true if other message ID matches this message ID, false otherwise
+     */
+    @Override
+    public boolean equals(@Nullable Object other) {
+        boolean result = false;
+        if (other instanceof ChatTestMessage) {
+            result = mMessageId == ((ChatTestMessage) other).mMessageId;
+        }
+        return result;
+    }
+}
